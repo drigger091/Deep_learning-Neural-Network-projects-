ea43783a35266d81d426580d8c24b248a9ab31f9
drigger091 <42235258+drigger091@users.noreply.github.com>
2023-06-09 23:06:16 +0530

    training the model

---
+++ implement.py
@@ -0,0 +1,24 @@
+from sklearn.model_selection import train_test_split
+import data_prep as dp
+import tensorflow as tf
+from tensorflow import keras
+from model import create_model
+
+
+X , Y = dp.get_XY()
+
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.25,random_state=2)
+
+# scale the data
+
+X_train_scaled = X_train/255
+X_test_scaled = X_test/255  
+
+model = create_model()
+
+model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['acc'])
+
+history = model.fit(X_train_scaled,Y_train,validation_split=0.1,epochs=6)
+              
+              
+
--- model.py
+++ model.py
@@ -1,10 +1,36 @@
-from sklearn.model_selection import train_test_split
-import data_prep as dp
+import tensorflow as tf
+from tensorflow import keras
 
 
-X , Y = dp.get_XY()
+# building the convulational neural network
 
-print('Number of Numpy arrays in X(data):',X.shape[0])
-print('Height of the arrays in X(data):',X.shape[1])
-print('Width of the arrays in X(data):',X.shape[2])
-print('Color codes in X(data):',X.shape[3])
+def create_model():
+    num_of_classes = 2
+
+    model = keras.Sequential()
+
+    try:
+        model.add(keras.layers.Conv2D(32, kernel_size=(3,3), activation='relu', input_shape=(128,128,3)))
+        model.add(keras.layers.MaxPooling2D(pool_size=(2,2)))
+
+        model.add(keras.layers.Conv2D(64, kernel_size=(3,3), activation='relu'))
+        model.add(keras.layers.MaxPooling2D(pool_size=(2,2)))
+
+        model.add(keras.layers.Flatten())
+
+        model.add(keras.layers.Dense(128, activation='relu'))
+        model.add(keras.layers.Dropout(0.5))
+
+        model.add(keras.layers.Dense(64, activation='relu'))
+        model.add(keras.layers.Dropout(0.5))
+
+        model.add(keras.layers.Dense(num_of_classes, activation='sigmoid'))
+
+        return model
+
+    except ValueError as ve:
+        print("ValueError occurred:", str(ve))
+    except TypeError as te:
+        print("TypeError occurred:", str(te))
+    except Exception as e:
+        print("An error occurred:", str(e))
