7961b592c4cb8ebf082e949f1a4a7d98dce9c4d0
drigger091 <42235258+drigger091@users.noreply.github.com>
2023-06-11 19:43:46 +0530

    update

--- predictor.py
+++ predictor.py
@@ -2,31 +2,26 @@ import cv2
 import numpy as np
 from tensorflow import keras
 
-# Load the trained model
+
 model = keras.models.load_model('trained_model.h5')
 
-# Setup webcam
 video_Capture = cv2.VideoCapture(0)
 
-# Class labels
 class_labels = ['Mask', 'Without Mask']
 
-# Process the webcam frames
+
 while True:
     ret, frame = video_Capture.read()
 
-    # Resize the frame
+    
     frame = cv2.resize(frame, (128, 128))
 
-    # Preprocess the frame
-    frame = frame / 255.0  # Normalize the pixel values
+    frame = frame / 255.0  
 
-    # Perform prediction
     predictions = model.predict(np.expand_dims(frame, axis=0))
     predicted_class_index = np.argmax(predictions[0])
     predicted_class_label = class_labels[predicted_class_index]
 
-    # Display the predicted class label on the frame
     cv2.putText(frame, predicted_class_label, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
     cv2.imshow("webcam", frame)
 
