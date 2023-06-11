15ba64d3bad1f11b34871f0ee10636e401fca8b2
drigger091 <42235258+drigger091@users.noreply.github.com>
2023-06-11 19:41:12 +0530

    update

--- predictor.py
+++ predictor.py
@@ -1,46 +1,37 @@
 import cv2
 import numpy as np
 from tensorflow import keras
-import pickle
 
+# Load the trained model
+model = keras.models.load_model('trained_model.h5')
 
-with open("training_history.pkl",'rb') as file:
-
-    model = pickle.load(file)
-
-
-# setup webcam
-
+# Setup webcam
 video_Capture = cv2.VideoCapture(0)
 
+# Class labels
+class_labels = ['Mask', 'Without Mask']
 
-#class labels
-class_labels = ['Mask','Without Mask']
-
-#process the webcam frames
-
+# Process the webcam frames
 while True:
+    ret, frame = video_Capture.read()
 
-    ret , frame = video_Capture.read()
+    # Resize the frame
+    frame = cv2.resize(frame, (128, 128))
 
-    # resize the frame
-    frame = cv2.resize(frame,(128,128))
+    # Preprocess the frame
+    frame = frame / 255.0  # Normalize the pixel values
 
-    
-    predictions = model.predict(np.expand_dims(frame,axis=0))
-
+    # Perform prediction
+    predictions = model.predict(np.expand_dims(frame, axis=0))
     predicted_class_index = np.argmax(predictions[0])
     predicted_class_label = class_labels[predicted_class_index]
 
+    # Display the predicted class label on the frame
+    cv2.putText(frame, predicted_class_label, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    cv2.imshow("webcam", frame)
 
-    cv2.putText(frame,predicted_class_label,(10,30),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
-    cv2.imshow("webcam",frame)
-
-
-    if cv2.waitKey(1) & 0xFF  == ord('q'):
-
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
 
 video_Capture.release()
 cv2.destroyAllWindows()
